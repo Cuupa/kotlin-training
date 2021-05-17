bf44c9532c34b3d1bd8cce8c9f7277610662e3b4
Cuupa <simonthiel2@gmail.com>
2021-05-17 19:56:19 +0200

    inner classes etc

---
+++ src/main/java/innerclasses/MyOuterClass.kt
@@ -0,0 +1,11 @@
+package innerclasses
+
+/**
+ * @author Simon Thiel (https://github.com/cuupa) (17.05.2021)
+ */
+class MyOuterClass {
+    private val myInt: Int = 1
+    class MyInnerClass {
+        val doMagic = 5
+    }
+}
